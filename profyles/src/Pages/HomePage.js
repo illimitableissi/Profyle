4ce2d2489d32dc51236c4d243343cbc68b65f83c
SOVEREIGN <isimemeni@gmail.com>
2020-02-28 14:08:39 -0500

    added more homepage changes

--- profyles/src/Pages/HomePage.js
+++ profyles/src/Pages/HomePage.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 import Image from 'react-bootstrap/Image'
 import Carousel from 'react-bootstrap/Carousel'
 import Navbar from 'react-bootstrap/Navbar'
@@ -27,13 +28,23 @@ const HomePage = () => {
                 <Nav className="mr-auto">
                 </Nav>
                 </Navbar>
+                <br></br>
             <Container fluid>
                 <Row>
-                    <Col>
+                    <Col xs={12} md={4}>
+                    <Image className="profilePic" src="/images/landscape2.jpg" roundedCircle fluid/>
+                    </Col>
+                    <Col xs={12} md={8}>
+                    <h1>Summary</h1>
+                    <hr></hr>
+                    <p>                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nobis sequi, corporis optio labore nisi sapiente accusamus vitae et quam illo, impedit inventore eveniet voluptatum necessitatibus provident autem incidunt eos.</p>
                     </Col>
                 </Row>
+                <hr></hr>
                 <Row>
                     <Col>
+                    <h2 className="text-center">Showcase your photos here:</h2>
+                    <br></br>
                 <Carousel>
                     <Carousel.Item>
                         <img
@@ -70,17 +81,55 @@ const HomePage = () => {
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel> 
+                <br></br>
                 </Col>
                 </Row>
+                <h2 className="text-center">Your Information:</h2>
+                <br></br>
                 <Row>
-                    <Col xs={12} md={4}>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nobis sequi, corporis optio labore nisi sapiente accusamus vitae et quam illo, impedit inventore eveniet voluptatum necessitatibus provident autem incidunt eos.</p>
+                    <Col xs={6} md={3}>
+                        <Card border="light">
+                            <Card.Header>Life Goals</Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk
+                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nobis sequi, corporis optio labore nisi sapiente accusamus vitae et quam illo, impedit inventore eveniet voluptatum necessitatibus provident autem incidunt eos.
+                                    </Card.Text>
+                            </Card.Body>
+                        </Card>
                     </Col>
-                    <Col xs={12} md={4}>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nobis sequi, corporis optio labore nisi sapiente accusamus vitae et quam illo, impedit inventore eveniet voluptatum necessitatibus provident autem incidunt eos.</p>
+                    <Col xs={6} md={3}>
+                    <Card border="light">
+                            <Card.Header>Notable Accomplishments</Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk
+                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nobis sequi, corporis optio labore nisi sapiente accusamus vitae et quam illo, impedit inventore eveniet voluptatum necessitatibus provident autem incidunt eos.
+                                    </Card.Text>
+                            </Card.Body>
+                        </Card>
                     </Col>
-                    <Col xs={12} md={4}>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nobis sequi, corporis optio labore nisi sapiente accusamus vitae et quam illo, impedit inventore eveniet voluptatum necessitatibus provident autem incidunt eos.</p>
+                    <Col xs={6} md={3}>
+                    <Card border="light">
+                            <Card.Header>Hobbies</Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk
+                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nobis sequi, corporis optio labore nisi sapiente accusamus vitae et quam illo, impedit inventore eveniet voluptatum necessitatibus provident autem incidunt eos.
+                                    </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        </Col>
+                        <Col xs={6} md={3}>
+                        <Card border="light">
+                            <Card.Header>Skills</Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk
+                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nobis sequi, corporis optio labore nisi sapiente accusamus vitae et quam illo, impedit inventore eveniet voluptatum necessitatibus provident autem incidunt eos.
+                                    </Card.Text>
+                            </Card.Body>
+                        </Card>
                     </Col>
                 </Row>
             </Container>  
