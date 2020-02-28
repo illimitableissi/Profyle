ea19965eced983eea28ba2ebf1e49f4348b06685
SOVEREIGN <isimemeni@gmail.com>
2020-02-27 21:06:55 -0500

    working on homepage

--- profyles/src/Pages/HomePage.js
+++ profyles/src/Pages/HomePage.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container'
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 
 const HomePage = () => {
     return (
         <div>
             <Container>
-                <Jumbotron>
-                <h1>Profyle</h1>
+                <Jumbotron className="text-center">
+                    <h1>Profyle</h1>
+                    <p>Profyle is a quick "About Me" page creation app. </p>
+                    <a href="/form" role="button" className="btn btn-primary">Create Your Page!</a>
                 </Jumbotron>
-            </Container>
-            
+                    <h5 className="text-center">See your page below</h5>
+            </Container>           
         </div>
     );
 };
