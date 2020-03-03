324766f215e310dc216181bc0234601321f8ded7
SOVEREIGN <isimemeni@gmail.com>
2020-03-03 00:19:39 -0500

    added back scripts

---
+++ scripts/seedDB.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb+srv://iinegbedion:8Q1tHdVLHOCdLCvV@cluster0-uuy8n.azure.mongodb.net/Profyle"
+);
+
+const userSeed = [
+    {
+    userName: "",
+    profilePic: "",
+    backgroundImg: "",
+    summary: "",
+    photoUrlOne: "",
+    photoOneCaption: "",
+    photoUrlTwo: "",
+    photoTwoCaption: "",
+    photoUrlThree: "",
+    photoThreeCaption: "",
+    goals: "",
+    accomplishments: "",
+    hobbies: "",
+    skills: "",
+}
+]
+
+
+db.User
+    .remove({})
+    .then(() => db.User.collection.insertMany(userSeed))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
